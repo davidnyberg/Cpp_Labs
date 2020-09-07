88383799f1d321399dae68160732454bd4660f65
David Nyberg <dnyberg95@gmail.com>
2020-09-07 20:06:49 +0200

    part2 done i think

--- Lab1/lab1_pt2.cpp
+++ Lab1/lab1_pt2.cpp
@@ -2,42 +2,7 @@
 
 using namespace std;
 
-//Checks the SEK inputs for negative values
-bool validateLast(float& input)
-{
-    if(input < 0)
-    {
-        cout << "ERROR: Price (SEK) must be at least 0" << endl;
-        return false;
-    }
-    return true;
-}
-
-//Checks the stride inputs for positive value
-bool validateStride(float& input)
-{
-    cout << input << endl;
-    if(input < static_cast<float>(0.01))
-    {
-        cout << "ERROR: Stride must be at least 0.01" << endl;
-        return false;
-    }
-    return true;
-}
-
-//Checks the stride inputs for positive value
-bool validateTaxRate(float& input)
-{
-    if(input < 0)
-    {
-        cout << "ERROR: Tax rate must be greater than 0" << endl;
-        return false;
-    }
-    return true;
-}
-
-int main()
-{
+int main() {
     float first_price;
     float last_price;
     float stride;
@@ -56,38 +21,42 @@ int main()
 
     cout << "Enter last price: ";
     cin >> last_price;
-    while(!validateLast(last_price))
-    {
+
+    //Checks the inputs for SEK is positive
+    while(last_price < 0) {
+        cout << "ERROR: Price (SEK) must be at least 0" << endl;
         cout << "Enter last price: ";
         cin >> last_price;
     }
 
     cout << "Enter stride: ";
     cin >> stride;
-    while(!validateStride(stride))
-    {
+    
+    //Checks that is greater than 0.01
+    while(stride <  static_cast<float>(0.01)) {
+        cout << "ERROR: Stride must be at least 0.01" << endl;
         cout << "Enter stride: ";
         cin >> stride;
     }
 
     cout << "Enter tax percent: ";
     cin >> tax_percent;
-    while(!validateTaxRate(tax_percent))
-    {
+    //Checks that tax percent is greater than zero
+    while(tax_percent < 0) {
+        cout << "ERROR: Tax rate must be greater than 0." << endl;
         cout << "Enter tax percent: ";
         cin >> tax_percent;
     }
 
     //testing
-    cout << first_price << last_price << stride << tax_percent << endl;
+    //cout << first_price << last_price << stride << tax_percent << endl;
 
     /*
     Print tax table
     */
     cout << "TAX TABLE\n==========\n"; 
     cout << "Price\tTax\tPrice With Tax\n-----------------------------" << endl;
-    for(;first_price <= last_price; first_price += stride)
-    {
+    for(;first_price <= last_price; first_price += stride) {
         float tax_amount = tax_percent/100 * first_price;
         float price_w_tax = tax_amount + first_price;
         cout << first_price << "\t"<< tax_amount << "\t" << price_w_tax <<endl;
