7c9149953982e09f75104898e091679f1de05cdd
davidnyberg <dnyberg95@gmail.com>
2020-12-08 12:32:12 +0100

    clean and validate done

--- Lab5/WordCollection.cpp
+++ Lab5/WordCollection.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <algorithm>
 #include <string>
+#include <iterator>
 using namespace std;
 
 
@@ -29,61 +30,57 @@ int main(int argc, char* argv[]) {
     }
 
 
-    //map<string, int> word_list;
-
     cout << "Opening: " << file_name << endl;
 
     ifstream file{file_name};
     
-    /*
-    istream_iterator<string> file_stream{file};
-    istream_iterator<string> end_of_stream;
-    vector<string> potential_words{file_stream, end_of_stream};
-    */
+    
+    istream_iterator<string> file_stream{file}, end;
+    vector<string> raw_words{file_stream, end};
 
-    vector<string> raw_words;
-    string word{};
-    while(file >> word) {
-        raw_words.push_back(word);
-    }
 
     vector<string> cleaned_words{};
     vector<string> validated_words{};
     
     //clean words
     transform(raw_words.begin(), raw_words.end(), back_inserter(cleaned_words), [] (auto& temp_word) -> string {
-
         //remove leading junk
         temp_word.erase(temp_word.begin(), find_if(temp_word.begin(), temp_word.end(), [](auto c) { return isalpha(c);}));
-
 
         //remove trailing junk, erase does not support reverse iterators, .base fixes this
         auto rit = find_if(temp_word.rbegin(), temp_word.rend(), [](auto c){ return isalpha(c);});
         temp_word.erase((rit).base(), temp_word.end());
 
+        //source: http://www.cplusplus.com/forum/general/49936/
+        if(temp_word.substr(temp_word.length() - 2) == "\'s") {
+            temp_word.erase(temp_word.length() - 2);
+        }
+
+
+
+        return temp_word;}
+    );
+    
+    //validate words
+    transform(cleaned_words.begin(), cleaned_words.end(), back_inserter(validated_words), [] (auto temp_word) -> string {
+
+        temp_word.erase(remove_if(temp_word.begin(), temp_word.end(), [temp_word](auto& c){ return (temp_word.find("--") != string::npos); }), temp_word.end());
+        
+        auto it = find_if(temp_word.begin(), temp_word.end(), [](auto& c){ return (!isalpha(c) && c != '-');});
+        if(it != temp_word.end()) {
+            temp_word.erase(temp_word.begin(), temp_word.end());
+        }
+
         return temp_word;}
     );
 
-    //cleaned_words.erase(find_if(temp_word.end(), temp_word.begin(), [](auto c){ return isalpha(c);}), temp_word.end())
-    
-    //remove short words >= 3 chars
-    cleaned_words.erase(remove_if(cleaned_words.begin(), cleaned_words.end(), [](auto& word){
-        return word.length() < 3; }), cleaned_words.end());
+   validated_words.erase(remove_if(validated_words.begin(), validated_words.end(), [](auto& word){
+        return word.length() < 3; }), validated_words.end());
 
-        for (auto word : cleaned_words) {
-        cout << word << " " << endl;
-    }
 
-    //remove word if it contains numbers
-    cleaned_words.erase(remove_if(cleaned_words.begin(), cleaned_words.end(), [](auto& word){
-        return any_of(word.begin(), word.end(), [](auto x) {
-            return !isalpha(x) && x != '-';
-        });
-    }), cleaned_words.end());
+    //now we have all the validated words in a vector
 
-   // transform(cleaned_words.begin(), cleaned_words.end(), back_inserter(validated_words), [] (auto& temp_word) -> string {}
-
-    for (auto word : cleaned_words) {
+    for (auto word : validated_words) {
         cout << word << " " << endl;
     }
     
